958a63694e4f4a5db1d3d3f5265cb17b0bc7b01c
onemoredavide <davideantipa@gmail.com>
2026-04-29 11:28:45 +0300

    update favicon

--- src/templates/layouts/partials/Head.tsx
+++ src/templates/layouts/partials/Head.tsx
@@ -31,13 +31,13 @@ export function Head({ globalResources }: Props) {
         rel="icon"
         type="image/png"
         sizes="32x32"
-        href="/favicon-32x32.png"
+        href="/apple-touch-icon.png"
       />
       <link
         rel="icon"
         type="image/png"
         sizes="16x16"
-        href="/favicon-16x16.png"
+        href="/apple-touch-icon.png"
       />
       <link rel="manifest" href="/site.webmanifest" />
 
